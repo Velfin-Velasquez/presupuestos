dd86ead2be6317975b74be8265827ec4d8ce1d40
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2020-12-10 18:03:50 -0500

    se creo formulario de ingreso de presupuesto y se valido la entrada de datos

---
+++ src/components/Pregunta.js
@@ -0,0 +1,47 @@
+import React, { Fragment, useState } from "react";
+import { Error } from "./Error";
+
+export const Pregunta = () => {
+  //definir states
+  const [cantidad, guardarCantidad] = useState(0);
+  const [error, seterror] = useState(false);
+
+  //funcion para definir presupuesto
+  const definirPresupuesto = (e) => {
+    e.preventDefault();
+    guardarCantidad(parseInt(e.target.value), 10);
+  };
+
+  //submit para agregar presupuesto
+  const agregarpresupuesto = (e) => {
+    e.preventDefault();
+
+    //validar
+    if (cantidad < 1 || isNaN(cantidad)) {
+      seterror(true);
+      return;
+    }
+    //si pasa la validacion
+    seterror(false);
+  };
+
+  return (
+    <Fragment>
+      <h2>Coloca tu presupuesto</h2>
+      {error ? <Error mensaje="El presupuesto es incorrecto" /> : null}
+      <form onSubmit={agregarpresupuesto}>
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="Coloca tu presupuesto"
+          onChange={definirPresupuesto}
+        />
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          value="Definir presupuesto"
+        />
+      </form>
+    </Fragment>
+  );
+};
